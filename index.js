060f1c3fca82993de98d870b7d4b18f923ca716b
Drew Covi <drew.covi@me.com>
2014-10-02 17:16:15 -0500

    temporarily removing sourcemaps

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ EmberCLIStylus.prototype.treeFor = function treeFor(type) {
 
 EmberCLIStylus.prototype.included = function included(app) {
   var options = app.options.stylusOptions || {};
-  if ((options.sourceMap === undefined) && (app.env == 'development')) {
-    options.sourcemap = {inline: true};
-  }
+  // if ((options.sourceMap === undefined) && (app.env == 'development')) {
+  //   options.sourcemap = {inline: true};
+  // }
   options.outputFile = options.outputFile || this.project.name() + '.css';
   app.registry.add('css', new StylusPlugin(options));
 };
