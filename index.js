f6ca49f978d0ce9c5b38f174b65a34b8e7489f3c
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-03-25 17:47:34 +0200

    Upgrade checker usage to match v2.1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var StylusCompiler = require('broccoli-stylus-single');
 var path = require('path');
-var checker = require('ember-cli-version-checker');
+var VersionChecker = require('ember-cli-version-checker');
 var mergeTrees = require('broccoli-merge-trees');
 
 function StylusPlugin(optionsFn) {
@@ -51,7 +51,10 @@ module.exports = {
   },
 
   shouldSetupRegistryInIncluded: function() {
-    return !checker.isAbove(this, '0.2.0');
+    var checker = new VersionChecker(this);
+    var emberVersion = checker.forEmber();
+
+    return !emberVersion.gt('0.2.0');
   },
 
   setupPreprocessorRegistry: function(type, registry) {
